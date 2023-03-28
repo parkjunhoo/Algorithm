e2fbd87a3e23edda64fae20a83dafc05f65280f6
Hoo <56852562+parkjunhoo@users.noreply.github.com>
2023-03-28 10:24:04 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,12 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        int count = s.nextInt();
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	try {
+			int count = Integer.parseInt(br.readLine());
+			for(int i =0; i<count; i++) {
+				String[] arr = br.readLine().split(" ");
+				System.out.println(Integer.parseInt(arr[0])+Integer.parseInt(arr[1]));
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
         
-        for(int i = 0; i < count; i++){
-            System.out.println(s.nextInt() + s.nextInt());
-        }
     }
 }
