585cf5158319019f7b3d16259aa9668346a990b7
Hoo <56852562+parkjunhoo@users.noreply.github.com>
2023-03-29 12:48:42 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        
+        if(x>0){
+            if(y>0){
+                System.out.print("1");
+            }else{
+                System.out.print("4");
+            }
+        }else{
+            if(y>0){
+                System.out.print("2");
+            }else{
+                System.out.print("3");
+            }
+        }
+    }
+}
