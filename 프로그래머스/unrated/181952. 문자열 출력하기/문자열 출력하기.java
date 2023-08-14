291e8652f0c5deeda37bca009bf95b10702c315e
Hoo <56852562+parkjunhoo@users.noreply.github.com>
2023-08-14 17:08:55 +0900

    [unrated] Title: 문자열 출력하기, Time: 136.22 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        int len = a.length();
+        if(len>= 1 && len <= 1000000) {
+            System.out.print(a);
+        }
+    }
+}
